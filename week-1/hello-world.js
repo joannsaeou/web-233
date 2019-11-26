7c7bcddca459b48e963f9a1766312bffca593299
Pascal Pascarella <pascalpascarella86@gmail.com>
2019-11-26 09:40:59 -0600

    Added Code Attribution

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,11 @@
 console.log("Hello World, my name is Pascal Pascarella!");
+
+/*
+============================================================
+; Assignment 1.3
+; Author: Pascal Pascarella
+; Date: 26 November 2019
+; Description: This program displays a "Hello World" message
+; with JavaScript.
+============================================================
+*/
