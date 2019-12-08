cc6273ba35105af2170326a718de4714e72453cf
Pascal Pascarella <pascalpascarella86@gmail.com>
2019-12-08 14:41:05 -0600

    Removed .vscode/launch.json

---
+++ week-2/pascarella-assignment-2.2.js
@@ -0,0 +1,10 @@
+
+/*
+============================================================
+; Assignment 2.2 - Header
+; Author: Pascal Pascarella
+; Date: 08 December 2019
+; Description: This program displays three employee records 
+; with JavaScript.
+============================================================
+*/
