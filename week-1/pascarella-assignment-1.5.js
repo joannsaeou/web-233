fab0a256cf6333f2bf68bca051b9690c50d7604f
Pascal Pascarella <pascalpascarella86@gmail.com>
2019-12-08 14:02:44 -0600

    Initial commit for Assignment-1.5.js

---
+++ week-1/pascarella-assignment-1.5.js
@@ -0,0 +1,48 @@
+var firstName = "Avleen";
+var lastName = "Burks";
+var address = "147 Sullivan Lane\n Chester, PA 19013";
+var payRate = 20.36;
+var hireDate = new Date(2019,03,11);
+
+console.log(
+    firstName,
+    lastName +'\n',
+    address +'\n',
+    "Hourly Pay: $" + payRate.toFixed(1) +'\n',
+    "Hired on " + hireDate.toLocaleDateString() +'\n');
+
+var firstName = "Mark";
+var lastName = "Romero";
+var address = "258 Harriette Drive\n Glendale, AZ 85302";
+var payRate = 19.45;
+var hireDate = new Date(2019,02,15);
+
+console.log(
+    firstName,
+    lastName +'\n',
+    address +'\n',
+    "Hourly Pay: $" + payRate.toFixed(1) +'\n',
+    "Hired on " + hireDate.toLocaleDateString() +'\n');
+
+var firstName = "Denzel";
+var lastName = "Parrish";
+var address = "369 Stanton Avenue\n Superior, WI 54880";
+var payRate = 18.49;
+var hireDate = new Date(2019,08,21);
+
+console.log(
+    firstName,
+    lastName +'\n',
+    address +'\n',
+    "Hourly Pay: $" + payRate.toFixed(1) +'\n',
+    "Hired on " + hireDate.toLocaleDateString() +'\n');
+    
+/*
+============================================================
+; Assignment 1.5 - Types, Values, and Variables
+; Author: Pascal Pascarella
+; Date: 08 December 2019
+; Description: This program displays three employee records 
+; with JavaScript.
+============================================================
+*/
