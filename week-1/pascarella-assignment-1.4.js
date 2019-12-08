a05b65add7a45a3562ccdb02c0188059e1f6ec71
Pascal Pascarella <pascalpascarella86@gmail.com>
2019-12-08 09:55:14 -0600

    Initial commit for Assignment-1.4.js

---
+++ week-1/pascarella-assignment-1.4.js
@@ -0,0 +1,18 @@
+var firstName = "Pascal";
+var lastName = "Pascarella";
+var age = "33";
+var street = "221B Baker Street";
+var city = "London";
+var zipCode = "NW1 6XE";
+
+console.log(firstName,lastName,age,street,city,zipCode);
+
+/*
+============================================================
+; Assignment 1.4 - Primitive Types
+; Author: Pascal Pascarella
+; Date: 08 December 2019
+; Description: This program displays a name and address 
+; with JavaScript.
+============================================================
+*/
