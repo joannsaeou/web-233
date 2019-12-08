de55a0399a6351bcb585c609907dca457093e5c9
Pascal Pascarella <pascalpascarella86@gmail.com>
2019-12-08 16:49:12 -0600

    Adding header and header-test

---
+++ pascarella-header.js
@@ -0,0 +1,20 @@
+/*
+============================================================
+; Title: pascarella-header.js
+; Author: Pascal Pascarella
+; Date: 08 December 2019
+; Description: This is a template header.
+============================================================
+*/
+
+/**
+ * Params: firstName, lastName, assignment
+ * Response: output
+ * Description: Returns a well-formatted string header
+ */
+exports.display = function (firstName, lastName, assignment) {
+  let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' +
+    new Date().toLocaleDateString()
+
+  return output
+}
---
+++ week-2/pascarella-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================================
+; Title: header-test.js
+; Author: Pascal Pascarella
+; Date: 08 December 2019
+; Description: This program displays a formatted header
+============================================================
+*/
+
+const header = require('../pascarella-header.js');
+
+console.log(header.display("Pascal","Pascarella","Week 2"));
