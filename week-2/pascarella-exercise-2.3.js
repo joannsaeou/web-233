cec3585c9935a9afd98e76aab1ba56d727310f11
Pascal Pascarella <pascalpascarella86@gmail.com>
2019-12-08 18:16:22 -0600

    Added exercise-2.3.js

---
+++ week-2/pascarella-exercise-2.3.js
@@ -0,0 +1,41 @@
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 2.3
+  Today's Date
+
+  Hello FirstName LastName!
+
+  Hint: Use your personal header display function and
+  refer to page 178 for implementing function properties
+*/
+/*
+============================================================
+; Title: Function Properties
+; Author: Pascal Pascarella
+; Date: 08 December 2019
+; Description: This program .
+============================================================
+*/
+
+// Header
+const header = require('../pascarella-header.js'); //Links to header file
+
+console.log(header.display("Pascal","Pascarella","Week 2")); //Prints header
+
+//Assign values to properties
+myName.pascal = "Pascal";
+myName.pascarella = " Pascarella";
+
+// Function declared
+function myName() {
+  return myName.pascal + myName.pascarella;
+}
+
+// Output
+console.log('\n');
+console.log("Hello, " + myName() + "!")
+
+
+// End Program
