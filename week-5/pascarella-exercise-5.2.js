7ee7158529b616918154c9e841cc91b4148a6091
Pascal Pascarella <pascalpascarella86@gmail.com>
2019-12-28 11:22:53 -0600

    Amended header

--- week-5/pascarella-exercise-5.2.js
+++ week-5/pascarella-exercise-5.2.js
@@ -1,24 +1,27 @@
 /*
 ============================================================
-; Title: Exercise 3. - Pattern Matching Functions
+; Title: Exercise 5.2 - ES5 Built-In Functions
 ; Author: Pascal Pascarella
-; Date: 14 December 2019
-; Description: This is a template header.
+; Date: 28 December 2019
+; Description: This is a program that iterates the forEach
+; function across a one-dimensional array.
 ============================================================
 */
 
 // Header
 const header = require('../pascarella-header.js');
 
-console.log(header.display("Pascal","Pascarella","Exercise 3.2 - Pattern Matching Functions"));
+console.log(header.display("Pascal","Pascarella","Exercise 5.2 - Built-In Functions"));
 
 
 // Start Program
 
+// Declare Variables
 var foods = ["Oysters", "Shrimp", "Steak", "Tacos", "Sushi"];
 
-foods.forEach(function(food) {
-    console.log(food);
+// Functions
+foods.forEach(function(food) {  // Apply function to each item in the array
+    console.log(food);          // Output to console
 });
 
 // End Program
